09b5437442303f5a9639627706bde7d233526717
lidijasuprina <lidija.suprina@creative-square.agency>
2023-06-05 14:53:40 +0200

    CSV delimiter changes & Code reorganization

--- file-processing.cpp
+++ file-processing.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <iomanip>
+#include <filesystem> 
 #include <vector>
 #include <algorithm>
-#include <filesystem> 
 
 using namespace std;
 
 const int COLUMN_WIDTH = 20;
-fstream file;
+const string OUTPUT_DIRECTORY = "output";
+const string ACCEPTED_EXTENSION = ".csv";
 
-class cperson {
+class Person {
     public:
         string firstName, lastName, dateOfBirth, firstNameHex, lastNameHex, dateOfBirthHex;
 
-        cperson(string first, string last, string date) : firstName(first), lastName(last), dateOfBirth(date) {
+        Person(string first, string last, string dob) : firstName(first), lastName(last), dateOfBirth(dob) {
             firstNameHex = stringToHex(firstName);
             lastNameHex = stringToHex(lastName);
             dateOfBirth = formatDate(dateOfBirth);
@@ -59,7 +59,7 @@ class cperson {
         }
 };
 
-string trimWhitespace(const string& str) {
+string trimWhitespace(string str) {
     size_t start = str.find_first_not_of(" \t\r\n");
     size_t end = str.find_last_not_of(" \t\r\n");
 
@@ -70,44 +70,35 @@ string trimWhitespace(const string& str) {
     return str.substr(start, end - start + 1);
 }
 
-int main() {
-    string filePath, directoryPath = "output", acceptExt = ".csv", ext;
-    bool fileOpened = false;
-    int maxAttempts = 3, attempts = 0;
-
-    while (!fileOpened && attempts < maxAttempts) {
-        cout << "Unesite putanju do ulazne datoteke: ";
-        cin >> filePath;
-        ext = filesystem::path(filePath).extension().string();
-        attempts++;
-
-        if (!filesystem::exists(filePath)) {
-            cerr << "Datoteka '" << filePath << "' ne postoji." << endl;
-        } else if (!ifstream(filePath)) {
-            cerr << "Datoteku '" << filePath << "' se ne moze otvoriti." << endl;
-        } else if (acceptExt != ext) {
-            cerr << "Datoteka '" << filePath << "' ima pogresan nastavak (prihvaca se samo CSV datoteka)." << endl;
-        } else {
-            file.open(filePath, ios::in|ios::binary);
-            if (!file) {
-                cerr << "Nije uspjelo otvaranje datoteke!" << endl;
-            } else {
-                cout << endl;
-                fileOpened = true;
-                attempts = 0;
-            }
-        }
-    };
-
-    if (!fileOpened) {
-        cerr << "Nije uspjelo otvaranje datoteke nakon pokusaja " << maxAttempts << ". Izlaz..." << endl;
-        return 1;
+bool validateFile(string filePath) {
+    if (!filesystem::exists(filePath)) {
+        cerr << "Datoteka '" << filePath << "' ne postoji." << endl;
+        return false;
     }
 
+    if (filesystem::is_directory(filePath)) {
+        cerr << "Putanja '" << filePath << "' predstavlja direktorij." << endl;
+        return false;
+    }
 
-    vector<cperson> people;
+    string ext = filesystem::path(filePath).extension().string();
+    if (ext != ACCEPTED_EXTENSION) {
+        cerr << "Datoteka '" << filePath << "' ima pogresan nastavak (prihvaca se samo CSV datoteka)." << endl;
+        return false;
+    }
+
+    return true;
+}
+
+vector<Person> readFromFile(string filePath) {
+    vector<Person> people;
+    ifstream file(filePath);
+    if (!file) {
+        cerr << "Nije uspjelo otvaranje datoteke!" << endl;
+        return people;
+    }
+
     string line;
-
     while (getline(file, line)) {
         line = trimWhitespace(line);
         if (line.empty()) {
@@ -117,7 +108,7 @@ int main() {
         istringstream ss(line);
         string firstName, lastName, dateOfBirth;
 
-        if (!getline(ss, firstName, ',') || !getline(ss, lastName, ',') || !getline(ss, dateOfBirth, ',')) {
+        if (!getline(ss, firstName, ';') || !getline(ss, lastName, ';') || !getline(ss, dateOfBirth, ';')) {
             cerr << "Error: Missing column(s) in line: " << line << endl;
             continue;
         }
@@ -127,48 +118,81 @@ int main() {
             continue;
         }
 
-        transform(firstName.begin(), firstName.end(), firstName.begin(),
-            [](unsigned char c) { return toupper(c); });
-        transform(lastName.begin(), lastName.end(), lastName.begin(),
-            [](unsigned char c) { return toupper(c); });
+        transform(firstName.begin(), firstName.end(), firstName.begin(), [](unsigned char c) { return toupper(c); });
+        transform(lastName.begin(), lastName.end(), lastName.begin(), [](unsigned char c) { return toupper(c); });
 
-        cperson person(firstName, lastName, dateOfBirth);
+        Person person(firstName, lastName, dateOfBirth);
         people.push_back(person);
     }
 
     file.close();
+    return people;
+}
 
-    cout << left << setw(COLUMN_WIDTH) << "First Name";
-    cout << left << setw(COLUMN_WIDTH) << "Last Name";
-    cout << left << setw(COLUMN_WIDTH) << "Date of Birth";
-    cout << left << setw(COLUMN_WIDTH) << "First Name (Hex)";
-    cout << left << setw(COLUMN_WIDTH) << "Last Name (Hex)";
-    cout << left << setw(COLUMN_WIDTH) << "Date of Birth (Hex)";
-    cout << endl;
-
-    for (const cperson& person : people) {
-        person.printDetails();
-    }
-
-    if (!filesystem::exists(directoryPath)) {
-        filesystem::create_directory("output");
-    }
-
-    ofstream outputFile("output/data.csv");
+void createNewFile(string filePath, vector<Person> people) {
+    ofstream outputFile(filePath);
     if (!outputFile) {
         cerr << "Error creating new CSV file." << endl;
-        return 1;
+        return;
     }
 
     for (const auto& person : people) {
-        outputFile << person.firstName << ","
-                << person.lastName << ","
-                << person.dateOfBirth << ","
-                << person.firstNameHex << ","
-                << person.lastNameHex << ","
-                << person.dateOfBirthHex << endl;
+        outputFile << person.firstName << ";"
+                    << person.lastName << ";"
+                    << person.dateOfBirth << ";"
+                    << person.firstNameHex << ";"
+                    << person.lastNameHex << ";"
+                    << person.dateOfBirthHex << endl;
     }
+
     outputFile.close();
+}
+
+void printHeader() {
+    cout << left << setw(COLUMN_WIDTH) << "Ime";
+    cout << left << setw(COLUMN_WIDTH) << "Prezime";
+    cout << left << setw(COLUMN_WIDTH) << "Datum rodenja";
+    cout << left << setw(COLUMN_WIDTH) << "Ime (hex)";
+    cout << left << setw(COLUMN_WIDTH) << "Prezime (hex)";
+    cout << left << setw(COLUMN_WIDTH) << "Datum rodenja (hex)";
+    cout << endl;
+}
+
+int main() {
+    string filePath;
+    bool fileOpened = false;
+    int maxAttempts = 3, attempts = 0;
+
+    while (!fileOpened && attempts < maxAttempts) {
+        cout << "Unesite putanju do ulazne datoteke: ";
+        cin >> filePath;
+        attempts++;
+
+        if (!validateFile(filePath)) {
+            continue;
+        }
+        
+        cout << endl;
+        fileOpened = true;
+        attempts = 0;
+    };
+
+    if (!fileOpened) {
+        cerr << "Nije uspjelo otvaranje datoteke nakon pokusaja " << maxAttempts << ". Izlaz..." << endl;
+        return 1;
+    }
+
+    vector<Person> people = readFromFile(filePath);
+
+    printHeader();
+    for (const Person& person : people) {
+        person.printDetails();
+    }
+
+    if (!filesystem::exists(OUTPUT_DIRECTORY)) {
+        filesystem::create_directory(OUTPUT_DIRECTORY);
+    }
+    createNewFile(OUTPUT_DIRECTORY + "/data.csv", people);
     
-    return 1;
+    return 0;
 }
